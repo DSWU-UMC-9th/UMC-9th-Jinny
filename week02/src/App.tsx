945c91f69bc25128db2a7f53ebe9a3ebccf0e10e
jinhyo <kjhyo0220@gmail.com>
2025-09-24 17:42:33 +0900

    ✨ Feat: TSX 문법 실습 (#3)

--- week02/src/App.tsx
+++ week02/src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  const nickname = "지니";
+  const tomato = "토마토";
+  const techStacks = ["REACT", "NEXT", "VUE", "SVELTE", "ANGULAR", "REACT-NATIVE"];
+
+  return (
+    <>
+      <strong className="school">덕성여자대학교</strong>
+      <p style={{ color: "gray", fontWeight: "bold", fontSize: "30px" }}>
+        {nickname}/김진효
+      </p>
+      <h1>{`${nickname}는 🍅${tomato}🍅를 좋아합니다.`}</h1>
+
+      <ul>
+        {techStacks.map((tech, idx) => (
+          <li key={idx}>{tech}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
