af973107641301a0348f98a2759325d97c049ea7
jinhyo <kjhyo0220@gmail.com>
2025-10-05 01:30:41 +0900

    ✨ Feat: 기본 라우터 설정하기 (#5)

--- week03-router/src/App.tsx
+++ week03-router/src/App.tsx
@@ -1,7 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+// 경로(path)와 화면(element) 정의
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>홈 페이지</h1>,
+  },
+  { path: "/movies", element: <h1>영화 페이지</h1> },
+]);
+
+// RouterProvider로 router 전달
 function App() {
-  return <></>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
