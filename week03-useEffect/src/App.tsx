dbfab54106f80048afe67951234fc685bd0d6048
jinhyo <kjhyo0220@gmail.com>
2025-10-05 02:34:26 +0900

    ✨ Feat: useEffect 심화 실습 (#5)
    클린업 함수 실습

--- week03-useEffect/src/App.tsx
+++ week03-useEffect/src/App.tsx
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 import MoviePage from "./Pages/MoviePage";
+import SearchPage from "./Pages/SearchPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("컴포넌트가 처음 마운트 될 때 실행");
+    // console.log("컴포넌트가 처음 마운트 될 때 실행");
   }, []);
 
   useEffect(() => {
-    console.log(`count 값이 ${count}로 변경될 때마다 실행`);
+    // console.log(`count 값이 ${count}로 변경될 때마다 실행`);
   }, [count]);
 
   const router = createBrowserRouter([
@@ -28,7 +29,10 @@ function App() {
             +1 증가
           </button>
           <button>
-            <Link to={"/movies"}> 영화 보러 가기</Link>
+            <Link to={"/movies"}>영화 보러 가기</Link>
+          </button>
+          <button>
+            <Link to={"/search"}>클린업 함수 기본 예제</Link>
           </button>
         </>
       ),
@@ -36,6 +40,10 @@ function App() {
     {
       path: "/movies",
       element: <MoviePage />,
+    },
+    {
+      path: "/search",
+      element: <SearchPage />,
     },
   ]);
 
