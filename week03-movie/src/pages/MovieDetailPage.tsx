308e45daa0f5bab27d471b310ec39ed99234338d
jinhyo <kjhyo0220@gmail.com>
2025-10-05 07:35:55 +0900

    ✨ Feat: 영화 감독/출연 연동 (#5)

--- week03-movie/src/pages/MovieDetailPage.tsx
+++ week03-movie/src/pages/MovieDetailPage.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import type { MovieDetail } from "../types/movie";
+import type { Cast, MovieCredit, MovieDetail } from "../types/movie";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 const MovieDetailPage = () => {
   const [movie, setMovie] = useState<MovieDetail | null>(null);
+  const [credits, setCredits] = useState<Cast[]>([]);
 
   const [isPending, setIsPending] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -36,6 +37,29 @@ const MovieDetailPage = () => {
     fetchMovies();
   }, []);
 
+  useEffect(() => {
+    const fetchCredit = async () => {
+      setIsPending(true);
+      try {
+        const { data } = await axios.get<MovieCredit>(
+          `https://api.themoviedb.org/3/movie/${movieId}/credits?language=ko-KR`,
+          {
+            headers: {
+              Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+            },
+          }
+        );
+        setCredits(data.cast);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    fetchCredit();
+  }, []);
+
   if (isError) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -53,32 +77,54 @@ const MovieDetailPage = () => {
       )}
 
       {!isPending && (
-        <div>
-          <div className="relative">
-            <img
-              src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-              alt={`${movie?.title}의 이미지`}
-              className="h-[300px] w-full object-cover"
-            />
-            <div className="absolute inset-0 bg-gradient-to-r from-black to-tranparent"></div>
+        <>
+          <div>
+            <div className="relative">
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+                alt={`${movie?.title}의 이미지`}
+                className="h-[350px] w-full object-cover"
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-black to-tranparent"></div>
 
-            <div className="absolute top-0 text-white w-1/2 p-4 flex flex-col gap-2">
-              <h1 className="text-2xl font-bold">{movie?.title}</h1>
+              <div className="absolute top-0 text-white w-1/2 px-4 py-8 flex flex-col gap-2">
+                <h1 className="text-2xl font-bold">{movie?.title}</h1>
 
-              <div className="leading-[20px]">
-                <p>평균 {movie?.vote_average.toFixed(2)}</p>
-                <p>{movie?.release_date.slice(0, 4)}</p>
-                <p>{movie?.runtime}분</p>
+                <div className="leading-[20px]">
+                  <p>평균 {movie?.vote_average.toFixed(2)}</p>
+                  <p>{movie?.release_date.slice(0, 4)}</p>
+                  <p>{movie?.runtime}분</p>
+                </div>
+
+                <p className="text-xl font-semibold">{movie?.tagline}</p>
+
+                <div className="line-clamp-4 ">{movie?.overview}</div>
               </div>
 
-              <p className="text-xl font-semibold">{movie?.tagline}</p>
-
-              <div className="line-clamp-4 ">{movie?.overview}</div>
+              <div className="absolute bottom-0 w-1/2 h-1 bg-gradient-to-r from-teal-500"></div>
             </div>
-
-            <div className="absolute bottom-0 w-1/2 h-1 bg-gradient-to-r from-teal-500"></div>
           </div>
-        </div>
+
+          <div>
+            <p className="font-bold text-2xl p-4">감독/출연</p>
+            <div className="flex flex-wrap gap-3">
+              {credits.slice(0, 20).map((credit) => (
+                <div
+                  key={credit.id}
+                  className="w-[200px] flex flex-col items-center justify-start gap-2"
+                >
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${credit.profile_path}`}
+                    alt={`${credit.name}의 이미지`}
+                    className="size-30 rounded-full"
+                  />
+                  <p className="font-semibold text-lg">{credit.name}</p>
+                  <p className="font-sm text-sm leading-[5px]">{credit.original_name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
       )}
     </>
   );
--- week03-movie/src/types/movie.ts
+++ week03-movie/src/types/movie.ts
@@ -73,3 +73,38 @@ export type MovieDetail = {
   vote_average: number;
   vote_count: number;
 };
+
+export type Cast = {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+};
+
+export type Crew = {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  credit_id: string;
+  department: string;
+  job: string;
+};
+
+export type MovieCredit = {
+  id: number;
+  cast: Cast[];
+  crew: Crew[];
+};
