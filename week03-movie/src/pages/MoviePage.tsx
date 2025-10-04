c0d9f8503444ef1f70ecbbad73ebe2c117bc1330
jinhyo <kjhyo0220@gmail.com>
2025-10-05 05:25:08 +0900

    💄 Design: 가운데 정렬 추가 (#5)

--- week03-movie/src/pages/MoviePage.tsx
+++ week03-movie/src/pages/MoviePage.tsx
@@ -23,7 +23,7 @@ const MoviePage = () => {
   }, []);
 
   return (
-    <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+    <div className="py-10 grid gap-4 place-items-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
       {movies?.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
