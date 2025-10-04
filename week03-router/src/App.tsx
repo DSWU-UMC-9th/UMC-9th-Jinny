5ca51f80dda65f3080f64e2bd6c7f1fc43d55615
jinhyo <kjhyo0220@gmail.com>
2025-10-05 01:34:10 +0900

    ✨ Feat: errorElement 설정 (#5)

--- week03-router/src/App.tsx
+++ week03-router/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 import "./App.css";
 
 // 경로(path)와 화면(element) 정의
@@ -8,6 +8,17 @@ const router = createBrowserRouter([
     element: <h1>홈 페이지</h1>,
   },
   { path: "/movies", element: <h1>영화 페이지</h1> },
+  {
+    path: "*",
+    element: (
+      <>
+        <h1>페이지를 찾을 수 없어요(404)</h1>
+        <button>
+          <Link to={"/"}>홈으로</Link>
+        </button>
+      </>
+    ),
+  },
 ]);
 
 // RouterProvider로 router 전달
