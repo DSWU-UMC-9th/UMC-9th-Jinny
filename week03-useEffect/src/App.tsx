13cfe074b8b3111865e677957945b03817255910
jinhyo <kjhyo0220@gmail.com>
2025-10-05 01:58:55 +0900

    ✨ Feat: useEffect 기초 (#5)

--- week03-useEffect/src/App.tsx
+++ week03-useEffect/src/App.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("컴포넌트가 처음 마운트 될 때 실행");
+  }, []);
+
+  useEffect(() => {
+    console.log(`count 값이 ${count}로 변경될 때마다 실행`);
+  }, [count]);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        +1 증가
+      </button>
+    </>
+  );
 }
 
 export default App;
