ffc92ecd4b9d47d14f940c434c2163d490c759a2
jinhyo <kjhyo0220@gmail.com>
2025-10-29 14:03:24 +0900

    🎉 Init: 프로젝트 세팅 (#9)

---
+++ week05-lp/src/App.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import HomeLayout from "./layouts/HomeLayout";
+import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
+
+// 1. 홈페이지
+// 2. 로그인 페이지
+// 3. 회원가입 페이지
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />, // 공유하는 레이아웃
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      { path: "signup", element: <SignupPage /> },
+      { path: "mypage", element: <MyPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
---
+++ week05-lp/src/pages/MyPage.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { getMyInfo } from "../apis/auth";
+import type { ResponseMyInfoDto } from "../types/auth";
+
+const MyPage = () => {
+  const [data, setData] = useState<ResponseMyInfoDto>();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getMyInfo();
+      setData(response);
+    };
+
+    getData();
+  }, []);
+
+  return <div>{data?.data.name}</div>;
+};
+
+export default MyPage;
