565eeb93b473a587f18bae326ceb8ea4ed143c7e
jinhyo <kjhyo0220@gmail.com>
2025-11-03 16:28:54 +0900

    💡 Init: 프로젝트 세팅 (#11)
    tanstack query 실습 프로젝트 세팅

---
+++ week06-tanstackQuery/src/App.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
