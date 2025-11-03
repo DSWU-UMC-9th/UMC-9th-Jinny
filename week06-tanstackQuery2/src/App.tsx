a8e215644008e6a3f668a77bc1b1817e99ab6c61
jinhyo <kjhyo0220@gmail.com>
2025-11-03 17:31:51 +0900

    ✨ Feat: 데이터 연결 (#11)

--- week06-tanstackQuery2/src/App.tsx
+++ week06-tanstackQuery2/src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 function App() {
-  return <></>;
+  const [data, setData] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+
+      const data = (await response.json()) as User;
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>Tanstack Query</h1>
+      {data?.name}
+    </>
+  );
 }
 
 export default App;
