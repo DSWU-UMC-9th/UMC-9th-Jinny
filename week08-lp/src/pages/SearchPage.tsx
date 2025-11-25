2849979f4f12e72a5dfe77aca2713f117e65383b
jinhyo <kjhyo0220@gmail.com>
2025-11-26 01:13:29 +0900

    ♻️ Refactor: 검색 부분 Debounce 활용하도록 개선 (#15)

--- week08-lp/src/pages/SearchPage.tsx
+++ week08-lp/src/pages/SearchPage.tsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
-import useGetLpList from "../hooks/queries/useGetLpList";
+import { useEffect, useState } from "react";
 import LpCard from "../components/LpCard";
 import { PAGINATION_ORDER } from "../enums/common";
 import LoadingSpinner from "../components/LoadingSpinner";
+import useGetInfiniteLpList from "../hooks/queries/useGetInfiniteLpList";
+import { useInView } from "react-intersection-observer";
+import LpCardSkeletonList from "../components/LpCardSkeletonList";
+import useDebounce from "../hooks/useDebounce";
+import { SEARCH_DEBOUNCE_DELAY } from "../constants/delay";
 
 const SearchPage = () => {
   const [search, setSearch] = useState("");
+  const debouncedValue = useDebounce(search, SEARCH_DEBOUNCE_DELAY);
   const [order, setOrder] = useState<PAGINATION_ORDER>(PAGINATION_ORDER.asc);
 
-  const { data, isPending, isError } = useGetLpList({ search, order });
+  const {
+    data,
+    isPending,
+    isError,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useGetInfiniteLpList({ limit: 10, search: debouncedValue, order });
+
+  // ref: 특정한 HTML 요소를 검사할 수 있음
+  // inView: 그 요소가 화면에 보이면 true
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (inView && !isFetching && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
 
   if (isPending) {
     return (
@@ -23,11 +48,17 @@ const SearchPage = () => {
   }
 
   return (
-    <div className="mt-20 w-full">
-      <div className="flex w-full justify-end pr-6">
+    <div className="p-3 w-full">
+      <div className="flex w-full justify-end pr-7 my-2">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="border"
+        />
+
         <button
           onClick={() => setOrder(PAGINATION_ORDER.asc)}
-          className={`rounded-l-sm px-3 py-2 cursor-pointer transition-all duration-300  hover:shadow-lg  ${
+          className={`rounded-l-sm px-3 py-2 w-20 cursor-pointer transition-all duration-300  hover:shadow-lg text-sm ${
             order === "asc"
               ? "bg-black text-white border border-black"
               : "bg-white border border-gray-300"
@@ -38,26 +69,27 @@ const SearchPage = () => {
 
         <button
           onClick={() => setOrder(PAGINATION_ORDER.desc)}
-          className={`rounded-r-sm px-3 py-2 cursor-pointer shadow-sm trnasition-all duration-300  hover:shadow-lg ${
+          className={`rounded-r-sm px-3 py-2 w-20 cursor-pointer shadow-sm trnasition-all duration-300  hover:shadow-lg text-sm ${
             order === "desc"
               ? "bg-black text-white border border-black"
               : "bg-white border border-gray-300"
           }`}
         >
-          오래된순
+          최신순
         </button>
       </div>
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className="border"
-      />
 
       <div className="flex flex-wrap gap-4 justify-center">
-        {data.map((lp) => (
-          <LpCard data={lp} />
-        ))}
+        {data.pages
+          .map((page) => page.data.data)
+          .flatMap((lps) => lps.map((lp) => <LpCard key={lp.id} data={lp} />))}
+
+        {(isFetching || isFetchingNextPage || isPending) && (
+          <LpCardSkeletonList count={20} />
+        )}
       </div>
+
+      <div ref={ref} className="mt-2 h-2"></div>
     </div>
   );
 };
